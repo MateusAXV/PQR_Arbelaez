e9ebf5eefdd5a4dfa60455b2b04e3ed48f337f50
05michelleC <michellecamelo50@gmail.com>
2023-11-05 13:39:46 -0500

    Actualizacion1
    Cambios en el login y registro.
    Creación de archivos para la conexión a la base de datos.
    Uso de nodemon.
    
    Co-Authored-By: dagarciagil <81365684+dagarciagil@users.noreply.github.com>
    Co-Authored-By: dev-danielgp <141603965+dev-danielgp@users.noreply.github.com>
    Co-Authored-By: Ana Ximena Vanegas Mateus <84426333+MateusAXV@users.noreply.github.com>

---
+++ js/conexion.js
@@ -0,0 +1,27 @@
+var mysql = require('mysql');
+
+var conexion = mysql.createConnection({
+
+    host: 'localhost',
+    database: 'pqrs_db',
+    user: 'root',
+    password: ''
+});
+
+conexion.connect(function (error) {
+
+    if (error) {
+        throw error;
+    } else {
+        console.log('Conexion exitosa');
+    }
+});
+
+conexion.query('SELECT * FROM registros', function (error, results, fields) {
+    if (error) throw error;   // Si hay un error en la consulta SQL
+    results.forEach(result => {
+        console.log(result)
+    });
+});
+
+conexion.end();
