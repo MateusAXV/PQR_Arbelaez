a6b05f53362b650ba6193d2498ebc687b0f75089
dagarciagil <diegoalexandergarciagil@gmail.com>
2023-11-10 14:39:40 -0500

    Inicio de sesión
    a

--- conexion.js
+++ conexion.js
@@ -1,10 +1,9 @@
 const express = require("express");
-const path = require ("path");
-
 const mysql = require("mysql");
 
 const app = express();
 
+const path = require("path");
 
 let conexion = mysql.createConnection({
   host: "localhost",
@@ -13,19 +12,25 @@ let conexion = mysql.createConnection({
   password: "",
 });
 
-app.set("view engine", "ejs"); //motor de vistas ejs
+app.listen(3000, function () {
+  console.log("Servidor creado http://localhost:3000");
+});
 
+app.set("view engine", "ejs"); //motor de vistas ejs
 // Configuración para servir archivos estáticos desde la carpeta 'public'
 app.use(express.static(path.join(__dirname, "/public")));
 
 //RUTA de página index
-app.get('/',(req,res)=>{
-res.render("index");
+app.get("/", (req, res) => {
+  res.render("index");
+});
+//Ruta de registro
+app.get("/registro", (req, res) => {
+  res.render("registro");
 });
 
-//Ruta de registro
-app.get('/registro', (req, res) => {
-  res.render("registro");
+app.get("/home", (req, res) => {
+  res.render("home");
 });
 
 // se usan para codificar los datos que mandan los html
@@ -33,22 +38,41 @@ app.use(express.json());
 //declarar las variables de un html para el js codificar los datos del html
 app.use(express.urlencoded({ extended: false }));
 
-app.listen(3000, function () {
-  console.log("Servidor creado http://localhost:3000");
+//////////CONSULTAS
+//Validar inicio de sesión //Ruta de llegada de datos para el login.
+app.post("/inicio", function (req, res) {
+  const datoslogin = req.body;
+  let numdoclogin = datoslogin.cedula_login;
+  let contralogin = datoslogin.pass_login;
+
+  let LoginQuery = `SELECT Numero_documento, Contraseña FROM registros WHERE Numero_documento = '${numdoclogin}' AND Contraseña = '${contralogin}';`;
+ 
+  conexion.query(LoginQuery, function (error, row) {
+    if (error) {
+      throw error;
+    } else {
+      if (row.length > 0) {
+        res.render("home");
+      } else {
+        res.render("index", {errorlogin: "Datos no válidos"});
+      }
+    }
+  });
 });
 
 //este llamado validar se usa para guardar las variables del form en estas variables locales.
+// Ruta para poder ingresar los datos
 app.post("/registrar", function (req, res) {
-  const datos = req.body;
+  const datosregistro = req.body;
 
-  let tipdoc = datos.tipodoc;
-  let numdoc = datos.numerodoc;
-  let nombCo = datos.nombreregistro;
-  let corr = datos.correo;
-  let celular = datos.celular;
-  let contra = datos.passwordlogin;
+  let tipdoc = datosregistro.tipodoc;
+  let numdoc = datosregistro.numerodoc;
+  let nombCo = datosregistro.nombreregistro;
+  let corr = datosregistro.correo;
+  let celular = datosregistro.celular;
+  let contra = datosregistro.passwordlogin;
 
-  let buscarprimarykey = "SELECT * FROM registros WHERE Numero_documento = " + numdoc + " ";
+  let buscarprimarykey = `SELECT Numero_documento, Contraseña FROM registros`;
 
   conexion.query(buscarprimarykey, function (error, row) {
     if (error) {
